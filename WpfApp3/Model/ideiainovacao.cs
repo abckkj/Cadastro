745844ab7d2e5f149af7327c117d9a8ad4c06f36
abckkj <126157069+abckkj@users.noreply.github.com>
2024-09-05 08:51:16 -0300

    Add files via upload

---
+++ WpfApp3/Model/ideiainovacao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.persistencia;
+
+namespace WpfApp3.Model
+{
+    internal class ideiainovacao
+    {
+        public string Area { get; set; } = "";
+
+        public string Ideia { get; set; } = "";
+
+        public float Custo { get; set; } = 0;
+
+        public Boolean CadastrarideiaInovacao(ideiainovacao i)
+        { 
+        
+        BD.SalvarBD(i);
+        
+        return true;
+        }
+    }
+}
---
+++ WpfApp3/persistencia/BD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.Model;
+
+namespace WpfApp3.persistencia
+{
+    internal class BD
+    {
+        public static List<ideiainovacao> mybd = new();
+
+        public static void SalvarBD(ideiainovacao i) => mybd.Add(i);
+
+        public static List<ideiainovacao> RetornarBD() => mybd;
+    }
+}
